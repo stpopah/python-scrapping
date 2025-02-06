5209ee1a0cabf6d3e31e43b61daaf836bebd8070
Jinho <stpopah@gmail.com>
2025-02-06 15:48:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.content, "html.parser")
 
+ads = soup.find_all("li", class_="feature--ad")
+for ad in ads:
+    ad.decompose()
+
 jobs = soup.find("section", class_="jobs").find_all("li")
-print(jobs)
+
+for job in jobs:
+    title = job.find("h4", class_= "new-listing__header__title").text
+    company_name = job.find("p", class_= "new-listing__company-name").text
+    print(title, "========", company_name)
